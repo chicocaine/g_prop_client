1d199c42590718b77737b84c88de3d9c0586e1a3
Jade Palma <jedzxcx1@gmail.com>
2025-02-22 23:56:51 +0800

    sign up page boiler

---
+++ resources/views/sign-up.blade.php
@@ -0,0 +1,37 @@
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script> 
+<section id="register" class="max-w-screen max-h-screen">
+<div class="max-w-screen max-h-screen flex ">
+
+  <div class="w-2/5 min-h-screen bg-white flex flex-col justify-center items-center">
+    <img src="logo.svg" alt="Logo">
+  <h1>
+  Sign In
+  </h1>
+  <form action="" class="flex flex-col my-10 mb-32">
+    <input class="my-8 w-[419px] h-[59px] rounded-[4px]" type="text" placeholder="Email">
+    <input class="mb-8 w-[419px] h-[59px] rounded-[4px]" type="password" placeholder="Password">
+    <div class="flex justify-start">
+      <button class="align-start bg-[#D9D9D9] rounded-[59px] w-[186px] h-[59px] mr-8">Sign In</button>
+      <div class="h-[59px] border-l-2 border-gray-300"></div>
+
+      <div class="flex justify-center items-center ml-10">
+        <a href="">
+        <h1 class="mr-6 text-[#61BCF9]">Sign Up</h1>
+        </a>
+        <div>
+          <h1>New Here?</h1>
+        </div>
+      </div>
+    </div>
+  </form>
+  </div>
+
+  <div class="w-3/5 min-h-screen bg-[#D9D9D9]">
+
+  </div>
+
+
+
+</div>
+
+</section>
