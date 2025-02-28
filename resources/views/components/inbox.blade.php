e2ae4491500b9fbe748475bd30e0bc48ebd9cc25
Jade Palma <jedzxcx1@gmail.com>
2025-02-28 15:32:22 +0800

    empty threads and commission placeholder

--- resources/views/components/inbox.blade.php
+++ resources/views/components/inbox.blade.php
@@ -1,12 +1,20 @@
 <div class="flex">
 
-<div class="ml-8 px-4 py-10 sm:pl-6 lg:py-4 w-[525px]">
+<div class="ml-8 mr-20 px-4  sm:pl-6  w-[525px]">
   <div class="flex flex-col">
-    <div class="-m-1.5 overflow-x-auto">
+    <div class="-m-1.5">
       <div class="p-1.5 min-w-full inline-block align-middle">
         <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-neutral-900 dark:border-neutral-700">
           <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
             <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+              @if ($commissions->isEmpty())
+                <div class="flex flex-col items-center justify-center align-center w-[555px] h-[763px] border border-gray-50 shadow-sm dark:border-neutral-700 rounded-[12px]">
+                  <img src="load.svg" alt="No conversation selected" class="w-[77px] h-[77px]" />
+
+                  <p class="my-8 text-gray-500 dark:text-neutral-400 font-bold">No Message Thread Created.</p>
+                  <p >Make a message now :)))</p>
+                </div>
+              @else
               @foreach ($commissions as $commission)
                 <tr class="bg-white hover:bg-gray-50 dark:bg-neutral-900 dark:hover:bg-neutral-800 cursor-pointer" onclick="showMessages({{ $commission->id }})">
                   <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-neutral-200">
@@ -17,6 +25,7 @@
                   </td>
                 </tr>
               @endforeach
+              @endif
             </tbody>
           </table>
         </div>
@@ -26,8 +35,14 @@
 </div>
 
 <div id="messages-container">
-  <!-- Messages will be loaded here -->
+  <div class="flex flex-col items-center justify-center align-center w-[555px] h-[763px] border border-gray-50 shadow-sm dark:border-neutral-700 rounded-[12px]">
+    <img src="load.svg" alt="No conversation selected" class="w-[77px] h-[77px]" />
+
+    <p class="my-8 text-gray-500 dark:text-neutral-400 font-bold">No Conversation Selected Yet.</p>
+    <p >Please Select a Commission Thread</p>
+  </div>
 </div>
+
 <script>
   function showMessages(commissionId) {
     fetch(`/commissions/${commissionId}/messages`)
