ec8eee87c06d8ce3b979587e56dd5ec8810041db
chicocaine <eduardanthony1208@gmail.com>
2025-02-28 18:01:15 +0800

    status naming convention

--- database/factories/ActionFactory.php
+++ database/factories/ActionFactory.php
@@ -20,7 +20,7 @@ class ActionFactory extends Factory
             'commission_id' => \App\Models\Commission::factory(),
             'name' => fake()->word(),
             'details' => fake()->sentence(),
-            'status' => fake()->randomElement(['pending', 'completed', 'in progress']),
+            'status' => fake()->randomElement(['pending', 'completed', 'active']),
             'deadline' => fake()->dateTimeThisYear()
         ];
     }
