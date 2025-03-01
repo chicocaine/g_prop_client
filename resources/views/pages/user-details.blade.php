8989b9fb57b8d738d0cde8444fc6b30cdae5279d
Jade Palma <jedzxcx1@gmail.com>
2025-03-02 01:35:48 +0800

    user details done

--- resources/views/pages/user-details.blade.php
+++ resources/views/pages/user-details.blade.php
@@ -1,48 +1,68 @@
 <script src="https://unpkg.com/@tailwindcss/browser@4"></script> 
 <x-nav-link />
 
-<body class="bg-gray-100 pt-10">
-    <div class="container mx-auto mt-10 p-5 bg-white rounded-lg shadow-lg">
-        <div class="px-4 sm:px-0">
-            <h3 class="text-lg font-semibold text-gray-900">User Information</h3>
-            <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details.</p>
+<body class="bg-white pt-10">
+    <div class="container mx-auto my-12 mt-40 p-5 bg-white rounded-lg flex flex-col items-center">
+        <div class="flex flex-col justify-center items-center px-4 sm:px-0">
+            <h3 class="text-[36px] font-semibold text-gray-900">Personal Information</h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">Info about your personal profile and security</p>
         </div>
-        <div class="mt-6 border-t border-gray-100">
-            <dl class="divide-y divide-gray-100">
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="text-sm font-medium text-gray-900">First Name</dt>
-                    <dd class="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->first_name }}</dd>
-                </div>
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="text-sm font-medium text-gray-900">Last Name</dt>
-                    <dd class="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->last_name }}</dd>
-                </div>
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="text-sm font-medium text-gray-900">Email</dt>
-                    <dd class="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->email }}</dd>
-                </div>
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="text-sm font-medium text-gray-900">Contact Number</dt>
-                    <dd class="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->contact_number }}</dd>
-                </div>
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="text-sm font-medium text-gray-900">Default Address</dt>
-                    <dd class="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->default_address }}</dd>
-                </div>
-            </dl>
+        <div class="mt-6 mb-6 p-12 border-t border-gray-100 shadow-lg flex flex-col space-y-4 rounded-[8px] w-3/7">
+            <h3 class="text-[26px] font-semibold text-gray-900">Basic Info</h3>
+            <div class="flex justify-between">
+                <h3 class="text-[16px] font-semibold text-[#666]">First Name</h3>
+                <h3 class="text-[16px] font-semibold text-black flex justify-start w-5/7">{{$user->first_name}}</h3>
+            </div>
+            <hr style="color: #B4B1B1;">
+    
+            <div class="flex justify-between">
+                <h3 class="text-[16px] font-semibold text-[#666]">Last Name</h3>
+                <h3 class="text-[16px] font-semibold text-black flex justify-start w-5/7">{{$user->last_name}}</h3>
+            </div>
+
+
+                
         </div>
-        <div class="flex justify-end mt-6">
-            <button id="openModal" class="bg-blue-500 text-white px-4 py-2 rounded">Edit Profile</button>
+         <div class="mt-6 p-12 border-t border-gray-100 shadow-lg flex flex-col space-y-4 rounded-[8px] w-3/7">
+            <h3 class="text-[26px] font-semibold text-gray-900">Contact Info</h3>
+            <div class="flex justify-between">
+                <h3 class="text-[16px] font-semibold text-[#666]">Email</h3>
+                <h3 class="text-[16px] font-semibold text-black flex justify-start w-5/7">{{$user->email}}</h3>
+            </div>
+            <hr style="color: #B4B1B1;">
+    
+            <div class="flex justify-between">
+                <h3 class="text-[16px] font-semibold text-[#666]">Phone</h3>
+                <h3 class="text-[16px] font-semibold text-black flex justify-start w-5/7">{{$user->contact_number}}</h3>
+            </div>
+            <hr style="color: #B4B1B1;">
+    
+            <div class="flex justify-between">
+                <h3 class="text-[16px] font-semibold text-[#666]">Address</h3>
+                <h3 class="text-[16px] font-semibold text-black flex justify-start w-5/7">{{$user->default_address}}</h3>
+            </div>
+
+
+                
         </div>
-        <form action="{{ route('logout') }}" method="POST" class="flex justify-end my-4">
+
+
+        <div class="flex justify-end mt-6 w-3/7 ">
+            <button id="openModal" class="min-w-full bg-white h-[60px] text-black text-[15px] px-4 py-2 rounded-[8px] shadow-sm hover:bg-gray-200">Edit Profile</button>
+        </div>
+        <div class="flex justify-end mt-2 w-3/7">
+            <form action="{{ route('logout') }}" method="POST" class="flex justify-end my-4 w-full">
             @csrf
-            <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded">Logout</button>
-        </form>
+            <button type="submit" class="bg-[#F66C73] text-white px-4 py-2 rounded-[8px] w-full h-[60px] hover:bg-[#e55a61]">Logout</button>
+            </form>
+        </div>
     </div>
+    
 
     <!-- Modal -->
-    <div id="updateModal" class="fixed inset-0 items-center justify-center flex bg-opacity-50 ">
-        <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-1/2">
+    <div id="updateModal" class="fixed inset-0 items-center justify-center flex  z-50 hidden">
+        <div class="fixed inset-0 bg-black opacity-50"></div> <!-- Overlay -->
+        <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-1/2 z-50">
             <div class="flex justify-between items-center mb-4">
                 <h3 class="text-lg font-semibold text-gray-900">Update Information</h3>
                 <button id="closeModal" class="text-gray-500 hover:text-gray-700">&times;</button>
