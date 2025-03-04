90a0e0e0dd6a7ec683a442e3b6c24bc5b312abb0
Jade Palma <jedzxcx1@gmail.com>
2025-03-04 20:49:13 +0800

    addded padding to input placeholders

--- resources/views/pages/sign-in.blade.php
+++ resources/views/pages/sign-in.blade.php
@@ -12,8 +12,8 @@
   </h1>
   <form action="{{ route('login') }}" method="POST" class="flex flex-col my-10 mb-32">
     @csrf
-    <input class="my-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="email" type="text" placeholder="Email">
-    <input class="mb-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]"  name="password" type="password" placeholder="Password">
+    <input class="my-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9] ps-4" name="email" type="text" placeholder="Email">
+    <input class="mb-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9] ps-4"  name="password" type="password" placeholder="Password">
     <div class="flex justify-start">
       <button type="submit" class="align-start bg-[#D9D9D9] rounded-[59px] w-[186px] h-[59px] mr-8">Sign In</button>
       <div class="h-[59px] border-l-2 border-gray-300"></div>
--- resources/views/pages/sign-up.blade.php
+++ resources/views/pages/sign-up.blade.php
@@ -3,20 +3,20 @@
 <div class="max-w-screen max-h-screen flex ">
 
     <div class="w-2/5 max-h-screen bg-white flex flex-col justify-center items-center">
-        <img src="logo.svg" alt="Logo">
-    <h1>
+        <img src="logo.svg" alt="Logo"  width="150px" height="150px">
+    <h1 class="font-bold text-[32px] mb-2">
     Sign Up
     </h1>
-    <form action="{{route('register')}}" class="flex flex-col my-10 mb-32 justify-center items-center" method="POST">
+    <form action="{{route('register')}}" class="flex flex-col  justify-center items-center mb-8" method="POST">
     @csrf
-        <input class="my-4 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="first_name" type="text" placeholder="First Name">
-        <input class="my-4 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="last_name" type="text" placeholder="Last Name">
-        <input class="my-4 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="email" type="email" placeholder="Email">
-        <input class="my-4 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="contact_number" type="text" placeholder="Mobile Number">
-        <input class="my-4 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="password" type="password" placeholder="Password">
-        <input class="my-4 mb-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9]" name="password_confirmation" type="password" placeholder="Confirm Password">
+        <input class="my-2 w-[419px] h-[50px] rounded-[4px] bg-[#D9D9D9] ps-4" name="first_name" type="text" placeholder="First Name">
+        <input class="my-2 w-[419px] h-[50px] rounded-[4px] bg-[#D9D9D9] ps-4" name="last_name" type="text" placeholder="Last Name">
+        <input class="my-2 w-[419px] h-[50px] rounded-[4px] bg-[#D9D9D9] ps-4" name="email" type="email" placeholder="Email">
+        <input class="my-2 w-[419px] h-[50px] rounded-[4px] bg-[#D9D9D9] ps-4" name="contact_number" type="text" placeholder="Mobile Number">
+        <input class="my-2 w-[419px] h-[50px] rounded-[4px] bg-[#D9D9D9] ps-4" name="password" type="password" placeholder="Password">
+        <input class="my-2 mb-8 w-[419px] h-[59px] rounded-[4px] bg-[#D9D9D9] ps-4" name="password_confirmation" type="password" placeholder="Confirm Password">
 
-        <div class="flex justify-center pb-24 ">
+        <div class="flex justify-center ">
         <button type="submit" class="align-start bg-[#D9D9D9] rounded-[59px] w-[186px] h-[59px] mr-4 ml-8">Sign Up</button>
         <div class="h-[59px] border-l-2 border-gray-300"></div>
 
